ec8a4b76a4afd6a18b64284523ca6058051089d6
Frank Ganske <frank.ganske@web.de>
2015-02-28 21:48:49 +0100

    Shut down the proxy on serious errors

--- src/main/java/org/littleshoot/proxy/mitm/Launcher.java
+++ src/main/java/org/littleshoot/proxy/mitm/Launcher.java
@@ -5,19 +5,24 @@ import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 
 public class Launcher {
 
-	public static void main(final String... args) throws Exception {
+	public static void main(final String... args) {
 		org.littleshoot.proxy.Launcher.pollLog4JConfigurationFileIfAvailable();
+		try {
+			final int port = 9090;
 
-		final int port = 9090;
+			System.out.println("About to start server on port: " + port);
+			HttpProxyServerBootstrap bootstrap = DefaultHttpProxyServer.bootstrapFromFile("./littleproxy.properties")
+					.withPort(port).withAllowLocalOnly(false);
 
-		System.out.println("About to start server on port: " + port);
-		HttpProxyServerBootstrap bootstrap = DefaultHttpProxyServer.bootstrapFromFile("./littleproxy.properties")
-				.withPort(port).withAllowLocalOnly(false);
+			bootstrap.withManInTheMiddle(new HostNameMitmManager());
 
-		bootstrap.withManInTheMiddle(new HostNameMitmManager());
+			System.out.println("About to start...");
+			bootstrap.start();
 
-		System.out.println("About to start...");
-		bootstrap.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 }
