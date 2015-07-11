ee3f268e931a6a1cc2c77bd64dc9df6c2ee7a752
Frank Ganske <frank.ganske@web.de>
2015-07-11 19:15:40 +0200

    Closes #6 by seting expiration date of certificates
    Use hundred years in the future instead of the maximum possible date,
    since this fails with Apple iOS 8 clients. Tested with a real device.

--- src/main/java/org/littleshoot/proxy/mitm/CertificateHelper.java
+++ src/main/java/org/littleshoot/proxy/mitm/CertificateHelper.java
@@ -80,8 +80,15 @@ public final class CertificateHelper {
     private static final Date NOT_BEFORE = new Date(
             System.currentTimeMillis() - 86400000L * 365);
 
-    /** The maximum possible value in X.509 specification: 9999-12-31 23:59:59 */
-    private static final Date NOT_AFTER = new Date(253402300799000L);
+    /**
+     * The maximum possible value in X.509 specification: 9999-12-31 23:59:59,
+     * new Date(253402300799000L), but Apple iOS 8 fails with a certificate
+     * expiration date grater than Mon, 24 Jan 6084 02:07:59 GMT (issue #6).
+     * 
+     * Hundred years in the future from starting the proxy should be enough.
+     */
+    private static final Date NOT_AFTER = new Date(
+            System.currentTimeMillis() + 86400000L * 365 * 100);
 
     private static final String SSL_CONTEXT_PROTOCOL = "TLS";
 
