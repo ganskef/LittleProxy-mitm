e234f833ffd4bc9929b2f1356e7a9a57175f0af1
George Kankava <george.kankava@devfactory.com>
2016-04-07 18:10:44 +0400

    Multiple code improvements - squid:S2259, squid:S1148, squid:CommentedOutCodeLine, squid:S2159, squid:S1192, squid:S1872

--- src/main/java/org/littleshoot/proxy/mitm/Launcher.java
+++ src/main/java/org/littleshoot/proxy/mitm/Launcher.java
@@ -5,8 +5,12 @@ import java.io.File;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.littleshoot.proxy.HttpProxyServerBootstrap;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Launcher {
+
+    private static final Logger log = LoggerFactory.getLogger(Launcher.class);
 
     public static void main(final String... args) {
         File log4jConfigurationFile = new File(
@@ -29,7 +33,7 @@ public class Launcher {
             bootstrap.start();
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
             System.exit(1);
         }
     }
